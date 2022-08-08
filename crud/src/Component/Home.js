77f48ce33d0f2c05f3f16930910de5993a09b051
Felipe <machadofelipe2016@outlook.com>
2022-08-08 17:21:29 -0300

    c3-create another code to adding about my structure

--- crud/src/Component/Home.js
+++ crud/src/Component/Home.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
 const Home = () => {
+    
+    
+    
     return(<>
     <h1>Home here</h1>
     <h3>all car here</h3>
+
+    <form>
+        <label>
+            <input type='text' />
+        </label>
+    </form>
     
     </>)
 }
