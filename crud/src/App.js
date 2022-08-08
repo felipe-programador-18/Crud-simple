e72d229857e25ec72591946043e4c98e6e056089
Felipe <machadofelipe2016@outlook.com>
2022-08-08 17:21:47 -0300

    c4-adding some routes

--- crud/src/App.js
+++ crud/src/App.js
@@ -6,15 +6,17 @@ import RegisterCarros from './Component/MeusCarr';
 import NavBar from './Component/navbar';
 
 function App() {
-  return ( <BrowserRouter> 
-       <NavBar/> 
-      <Routes>
+  return (<BrowserRouter> 
+    <div className='App'> 
+    <NavBar/>
+
+    <Routes>
         <Route path='/' element={<Home/>} /> 
         <Route path='/registercarros' element={<RegisterCarros/>} />
     
      </Routes>
 
-
+     </div>
 </BrowserRouter>
   );
 }
